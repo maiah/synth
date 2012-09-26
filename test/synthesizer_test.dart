6926f486bf2144700e4e791b1a39b73db2cd290d
maiah <jjmmacariola@gmail.com>
2012-09-26 15:11:55 +0800

    Updated unit test.

--- test/synthesizer_test.dart
+++ test/synthesizer_test.dart
@@ -1,26 +1,26 @@
-#import('../lib/src/synthesizer.dart');
+#import('../lib/synth.dart');
 
 main() {
   bool isMatched = Router.matchPathToRoute('GET', '/', 'GET', '/');
-  assert(true, isMatched);
+  asserts(true, isMatched);
 
   isMatched = Router.matchPathToRoute('GET', '/hey', 'GET', '/hey');
-  assert(true, isMatched);
+  asserts(true, isMatched);
 
   isMatched = Router.matchPathToRoute('GET', '/person', 'GET', '/person');
-  assert(true, isMatched);
+  asserts(true, isMatched);
 
   isMatched = Router.matchPathToRoute('GET', '/person/:name', 'GET', '/person/maiah');
-  assert(true, isMatched);
+  asserts(true, isMatched);
 
   isMatched = Router.matchPathToRoute('GET', '/employee/:name/department/:id', 'GET', '/employee/maiah/department/3/');
-  assert(true, isMatched);
+  asserts(true, isMatched);
 
   isMatched = Router.matchPathToRoute('GET', '/book/:id', 'GET', '/book');
-  assert(false, isMatched);
+  asserts(false, isMatched);
 }
 
-assert(var expected, var result) {
+asserts(var expected, var result) {
   if (expected != result)
     throw new Exception('$expected != $result');
 }
