30fbc4a7e232de937caa79ee2059ab705e2ee369
Maiah <jjmmacariola@gmail.com>
2012-10-19 17:30:07 +0800

    Added sample file upload sample.

---
+++ example/file_upload_server.dart
@@ -0,0 +1,53 @@
+#import('../lib/synth.dart');
+
+main() {
+  route('GET', '/upload', (req, res) {
+    res.write(
+        '''
+        <!DOCTYPE html>
+        <html>
+        <body>
+        <form enctype="multipart/form-data" action="/upload" method="POST">
+        <input type="text" name="username" placeholder="username" /><br />
+        <input type="password" name="password" placeholder="Password" /><br />
+        <input type="file" name="thefile" /><br />
+        <input type="submit" value="Upload" />
+        </form>
+        </body>
+        </html>
+        '''
+        );
+  });
+
+  route('POST', '/upload', (req, res) {
+    final List<int> data = new List<int>();
+
+    req.inputStream.onData = () {
+      data.addAll(req.inputStream.read());
+    };
+
+    req.inputStream.onClosed = () {
+      String dataString = new String.fromCharCodes(data);
+      List<String> contents = dataString.split('\n');
+
+      String webKitFormBoundary = contents[0];
+      String contentDisposition = contents[1];
+      String contentType = contents[2];
+
+      String fileBody = '';
+      for (int i = 4; i < (contents.length - 2); i++) {
+        String content = contents[i];
+        if (!content.startsWith(webKitFormBoundary)) {
+          fileBody = '$fileBody$content\n';
+        }
+      }
+
+      print(webKitFormBoundary);
+      res.write(dataString);
+      res.outputStream.close();
+    };
+  });
+
+  start(7000);
+  print('Listening on port 7000...');
+}
