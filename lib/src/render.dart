df5078953739f9f16989ffc8a4fa756c1543f700
Matthias Knoll <matthias@bravenewtalent.com>
2012-11-07 19:06:48 +0000

    generate path to template
    Signed-off-by: Matthias Knoll <matthias@bravenewtalent.com>

--- lib/src/render.dart
+++ lib/src/render.dart
@@ -3,12 +3,26 @@ part of synth;
 
 class Render {
   
+  const PATH_VIEW = '/views/';
+  const PATH_LAYOUT = '/views/layout/';
+  
+  // the passed in template
   String template;
   
-  Render(String this.template);
+  // returns the root path of the app
+  String get _basePath {
+    
+    var script = new File(new Options().script);
+    var directory = script.directorySync();
+    
+    return "${directory.path}/";
+  }
+  
+  Render( String this.template );
   
   String render() {
-    return 'render template: ${this.template}';
+
+    return 'render template: ${this._basePath}${this.template}';
   }
   
 }
