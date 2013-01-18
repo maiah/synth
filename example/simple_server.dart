43351fd3f7576e1a010e1c978741c5632b7af581
Aapo Romu <aapo.romu@eficode.com>
2013-01-18 13:51:35 +0200

    Cleaned up example

--- example/simple_server.dart
+++ example/simple_server.dart
@@ -2,17 +2,13 @@ import '../lib/synth.dart';
 
 void main() {
     synthInit(webRoot: 'example/webapp/');
-    route('GET', '/query', (req, res) => idolQueryServe(req, res));
+    route('GET', '/myroute', (req, res) => myRouteHandler(req, res));
 
     start(port: 7000);
     print('Listening on port 7000');
   }
 
-  void fileServe(req, res) {
-    res.write('Hello, World!');
-  }
-
-  void idolQueryServe(req, res) {
+  void myRouteHandler(req, res) {
     res.write('REST path ${req.path}');
   }
 
